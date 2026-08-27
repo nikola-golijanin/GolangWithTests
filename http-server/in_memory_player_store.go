df1b0e3d47e39d0f0b1cbfbc01220b0663a1b1f9
Nikola Golijanin <nikola.golijanin@blocknox.de>
2026-08-27 10:50:12 +0200

    Http Server - JSON, routing and embedding

--- http-server/in_memory_player_store.go
+++ http-server/in_memory_player_store.go
@@ -1,11 +1,19 @@
 package main
 
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
 type InMemoryPlayerStore struct {
 	store map[string]int
 }
 
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{store: map[string]int{}}
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
